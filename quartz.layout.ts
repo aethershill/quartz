6c2493fb799ca90cab2a1866a5087d78fa0220bf
Masa <aethershill@gmail.com>
2024-09-24 16:21:19 -0500

    Quartz sync: Sep 24, 2024, 4:21 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -27,7 +27,13 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.Explorer({
+      filterFn: (node) => {
+        // set containing names of everything you want to filter out
+        const omit = new Set(["Archive"])
+        return !omit.has(node.name.toLowerCase())
+      },
+    })),
   ],
   right: [
     Component.Graph(),
