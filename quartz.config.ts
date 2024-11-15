95c1dc225e28ba3ecbaf0092b039dc0bf99733cd
Masa <aethershill@gmail.com>
2024-11-15 00:08:08 -0700

    Quartz sync: Nov 15, 2024, 12:08 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -27,8 +27,8 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#CF9FFF",
-          lightgray: "#e5e5e5",
+          light: "#C3B1E1",
+          lightgray: "#E0B0FF",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
